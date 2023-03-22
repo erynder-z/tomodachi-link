5206d843dc834a9e72341a9f63076878c05743b4
erynder-z <stfn.bgr@gmail.com>
2023-03-22 18:03:44 +0100

    Update profile card

--- src/components/Main/ProfileCard/ProfileCard.tsx
+++ src/components/Main/ProfileCard/ProfileCard.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import { UserDataType } from '../../../types/userDataType';
 
-export default function ProfileCard() {
+type Props = {
+    userData: UserDataType | null;
+};
+
+export default function ProfileCard({ userData }: Props) {
+    const { first_name, last_name, userpic, friends } = userData || {};
+    const numberOfFriends = friends?.length;
+    const base64String = btoa(
+        String.fromCharCode(...new Uint8Array(userpic?.data?.data))
+    );
+
     return (
-        <div className="flex w-full mr-4">
+        <div className="flex w-full">
             <div className="w-full text-center rounded-md shadow-lg p-4 bg-white">
                 <img
                     className="w-20 h-20 object-cover rounded-full mx-auto shadow-lg"
-                    src="https://images.unsplash.com/photo-1611342799915-5dd9f1665d04?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                    src={`data:image/png;base64,${base64String}`}
                     alt="User avatar"
                 />
-                <p className="font-semibold text-lg mb-5">Username</p>
-                <p className="text-sm mb-2">69 Friends</p>
-                <p className="text-sm">Status: Idle</p>
+                <p className="font-semibold text-sm my-5 break-all">
+                    {first_name} {last_name}
+                </p>
+                <p className="text-sm mb-2">{numberOfFriends} Friends</p>
             </div>
         </div>
     );
