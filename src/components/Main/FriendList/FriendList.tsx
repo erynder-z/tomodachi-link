92f9c9fd1d0ed2ac6fdf8d8c6117854dc4110d37
erynder-z <stfn.bgr@gmail.com>
2023-04-09 14:52:35 +0200

    Rename FriendList > UserList

--- src/components/Main/FriendList/FriendList.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function FriendList() {
-    return <div className="w-full bg-card">FriendList</div>;
-}
---
+++ src/components/Main/UserList/UserList.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import UserListItem from './UserListItem/UserListItem';
+import LoadingSpinner from '../../LoadingSpinner/LoadingSpinner';
+import useAuth from '../../../hooks/useAuth';
+import useInfoOverlay from '../../../hooks/useInfoOverlay';
+import { fetchSomeUsers } from '../../../utilities/fetchSomeUsers';
+import { UserListUserType } from '../../../types/userListTypes';
+
+export default function UserList() {
+    const { token, authUser } = useAuth();
+    const { setInfo } = useInfoOverlay();
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const handleFetchUsers = async () => {
+        if (authUser && token) {
+            setLoading(true);
+            await fetchSomeUsers(token, setUsers, setInfo);
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        handleFetchUsers();
+    }, []);
+
+    const userList = users?.map((user: UserListUserType) => (
+        <UserListItem key={user._id} listItemData={user} />
+    ));
+    return (
+        <div className="flex flex-col w-full p-4 bg-card">
+            <h1 className="text-center font-bold">People you may know</h1>
+            {userList}
+            {loading && (
+                <div className="flex justify-center items-center w-full py-4 ">
+                    <LoadingSpinner />
+                </div>
+            )}
+        </div>
+    );
+}
