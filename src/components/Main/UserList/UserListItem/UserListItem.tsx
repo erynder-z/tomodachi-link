a32dba13f658fb08242991c078e9832f4a813a5a
erynder-z <stfn.bgr@gmail.com>
2023-04-10 16:08:01 +0200

    Create link to user page

--- src/components/Main/UserList/UserListItem/UserListItem.tsx
+++ src/components/Main/UserList/UserListItem/UserListItem.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { UserListUserType } from '../../../../types/userListTypes';
+import { Link } from 'react-router-dom';
 
 type Props = {
     listItemData: UserListUserType;
 };
 
 export default function UserListItem({ listItemData }: Props) {
-    const { first_name, last_name, userpic } = listItemData || {};
+    const { _id, first_name, last_name, userpic } = listItemData || {};
 
     return (
-        <div className="flex items-center w-full gap-4 py-2">
+        <Link
+            to={`/users/${_id}`}
+            className="flex items-center w-full gap-4 py-2"
+        >
             <img
                 className="w-8 h-8 object-cover rounded-full"
                 src={`data:image/png;base64,${userpic.data}`}
                 alt="User avatar"
             />
             {first_name} {last_name}
-        </div>
+        </Link>
     );
 }
