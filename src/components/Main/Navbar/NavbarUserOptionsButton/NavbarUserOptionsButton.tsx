21a3471ba498a9f14d96188d6fabf785e34f8a44
erynder-z <stfn.bgr@gmail.com>
2023-04-12 18:22:26 +0200

    Display notification
    On mobile devices,
    display notification in navbar if friend requests are pending

--- src/components/Main/Navbar/NavbarUserOptionsButton/NavbarUserOptionsButton.tsx
+++ src/components/Main/Navbar/NavbarUserOptionsButton/NavbarUserOptionsButton.tsx
@@ -3,17 +3,22 @@ import useUserData from '../../../../hooks/useUserData';
 
 export default function NavbarUserOptionsButton() {
     const { userData } = useUserData();
-    const { userpic } = userData || {};
-    const base64String = btoa(
+    const { userpic, pending_friend_requests } = userData || {};
+    const base64String = window.btoa(
         String.fromCharCode(...new Uint8Array(userpic?.data?.data))
     );
+    const pendingFriendRequests = pending_friend_requests?.length;
+
     return (
-        <div>
+        <div className="relative">
             <img
                 className="w-8 h-8 object-cover rounded-full mx-auto shadow-lg"
                 src={`data:image/png;base64,${base64String}`}
                 alt="User avatar"
             />
+            {pendingFriendRequests ? (
+                <div className="absolute top-5 left-5 flex items-center justify-center h-4 w-4 rounded-full bg-red-500 text-white text-xs pointer-events-none"></div>
+            ) : null}
         </div>
     );
 }
