04d9c12c38a0c8c935b7cb31e9944c3a5f49fff5
erynder-z <stfn.bgr@gmail.com>
2023-04-12 21:38:21 +0200

    handle accepting friend requests

---
+++ src/utilities/acceptFriendRequest.tsx
@@ -0,0 +1,43 @@
+import { InfoType } from '../types/infoType';
+import { FaExclamationTriangle, FaRegSmile } from 'react-icons/fa';
+import { handleFetchErrors } from './handleFetchErrors';
+
+export const acceptFriendRequest = async (
+    token: string,
+    currentUserId: string,
+    requestUserId: string,
+    setInfo: (info: InfoType | null) => void
+) => {
+    try {
+        const serverURL = import.meta.env.VITE_SERVER_URL;
+        const requestBody = {
+            currentUserId,
+            requestUserId,
+        };
+        const response = await fetch(
+            `${serverURL}/api/v1/users/${requestUserId}/acceptFriendRequest`,
+            {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify(requestBody),
+            }
+        );
+
+        if (!response.ok) {
+            handleFetchErrors(response, setInfo);
+        }
+
+        setInfo({
+            message: 'Friend request accepted!',
+            icon: <FaRegSmile />,
+        });
+    } catch (err: unknown) {
+        setInfo({
+            message: 'Unable to accept friend request!',
+            icon: <FaExclamationTriangle />,
+        });
+    }
+};
