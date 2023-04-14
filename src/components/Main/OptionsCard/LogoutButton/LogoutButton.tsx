00a6dda18f8af8d833c5983a3ff931504e4e60bb
erynder-z <stfn.bgr@gmail.com>
2023-04-14 21:09:47 +0200

    Rework logout

--- src/components/Main/OptionsCard/LogoutButton/LogoutButton.tsx
+++ src/components/Main/OptionsCard/LogoutButton/LogoutButton.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { TbLogout } from 'react-icons/tb';
 import useAuth from '../../../../hooks/useAuth';
+import { redirect, useNavigate } from 'react-router-dom';
 
 export default function LogoutButton() {
     const { logout } = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogoutClick = () => {
+        navigate('/');
+        logout();
+    };
+
     return (
         <button
             type="button"
-            onClick={logout}
+            onClick={handleLogoutClick}
             className="cursor-pointer hover:drop-shadow-md hover:text-red-500"
         >
             <TbLogout size="1.5em" />
