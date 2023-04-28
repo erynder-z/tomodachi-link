a7556fd4d6e0152042138c9bd28463bc6640ab0f
erynder-z <stfn.bgr@gmail.com>
2023-04-28 21:56:21 +0200

    Display logout message

--- src/components/Main/OptionsCard/LogoutButton/LogoutButton.tsx
+++ src/components/Main/OptionsCard/LogoutButton/LogoutButton.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { TbLogout } from 'react-icons/tb';
 import useAuth from '../../../../hooks/useAuth';
 import { useNavigate } from 'react-router-dom';
+import { MdOutlineWavingHand } from 'react-icons/md';
+import useInfoCard from '../../../../hooks/useInfoCard';
 
 export default function LogoutButton() {
     const { logout } = useAuth();
+    const { setInfo } = useInfoCard();
     const navigate = useNavigate();
 
     const handleLogoutClick = () => {
         navigate('/');
+        setInfo({
+            typeOfInfo: 'good',
+            message: 'Logged out!',
+            icon: <MdOutlineWavingHand />,
+        });
         logout();
     };
 
