dca8fe036128601ab90f62d81befb2a5c88357fa
erynder-z <stfn.bgr@gmail.com>
2023-05-03 18:41:35 +0200

    Change color palette
    When image is monochrome, return a lighter shade of that color.

--- src/utilities/getColors.ts
+++ src/utilities/getColors.ts
@@ -4,16 +4,23 @@ import tinycolor from 'tinycolor2';
 export const getColors = (image: string) => {
     return extractColors(image)
         .then((colors) => {
-            const newPalette = colors.map((color) => {
-                const { h, s, l } = tinycolor(color.hex).toHsl();
-                const desaturated = tinycolor({
-                    h,
-                    s: s * 0.75,
-                    l,
-                }).toHexString();
-                return { ...color, hex: desaturated };
-            });
-            return newPalette;
+            if (colors.length === 1) {
+                const lighterColor = tinycolor(colors[0].hex)
+                    .lighten(25)
+                    .toHexString();
+                return [{ hex: lighterColor }];
+            } else {
+                const newPalette = colors.map((color) => {
+                    const { h, s, l } = tinycolor(color.hex).toHsl();
+                    const desaturated = tinycolor({
+                        h,
+                        s: s * 0.75,
+                        l,
+                    }).toHexString();
+                    return { ...color, hex: desaturated };
+                });
+                return newPalette;
+            }
         })
         .catch(console.error);
 };
