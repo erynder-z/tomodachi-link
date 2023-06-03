9864047ea796b5d2a0b9c4187d98a8e8b939c5b8
erynder-z <stfn.bgr@gmail.com>
2023-06-03 19:36:10 +0200

    Add delayed unmount hook

---
+++ src/hooks/useDelayUnmount.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+function useDelayUnmount(isMounted: boolean, delayTime: number) {
+    const [showElement, setShowElement] = useState<boolean>(false);
+    useEffect(() => {
+        let timeoutId: ReturnType<typeof setTimeout>;
+        if (isMounted && !showElement) {
+            setShowElement(true);
+        } else if (!isMounted && showElement) {
+            timeoutId = setTimeout(() => setShowElement(false), delayTime);
+        }
+        return () => clearTimeout(timeoutId);
+    }, [isMounted, delayTime, showElement]);
+    return showElement;
+}
+
+export default useDelayUnmount;
