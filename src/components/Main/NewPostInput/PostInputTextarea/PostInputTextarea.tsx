9931b99d978490e80ce718cd1b07e0277166fdbb
erynder-z <stfn.bgr@gmail.com>
2023-06-06 20:50:14 +0200

    Auto resize textarea

--- src/components/Main/NewPostInput/PostInputTextarea/PostInputTextarea.tsx
+++ src/components/Main/NewPostInput/PostInputTextarea/PostInputTextarea.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type PostInputTextareaProps = {
     postText: string;
@@ -13,24 +13,49 @@ export default function PostInputTextarea({
     handleNewPostChange,
     username,
 }: PostInputTextareaProps) {
+    const [textareaRows, setTextareaRows] = useState(1);
+    const [isTextareaEmpty, setIsTextareaEmpty] = useState(postText === '');
+
+    const handleTextareaChange = (
+        event: React.ChangeEvent<HTMLTextAreaElement>
+    ) => {
+        handleNewPostChange(event);
+        autoResizeTextarea(event.target);
+        setIsTextareaEmpty(event.target.value === '');
+    };
+
+    const autoResizeTextarea = (textarea: HTMLTextAreaElement) => {
+        textarea.style.height = 'auto';
+        textarea.style.height = `${textarea.scrollHeight}px`;
+        setTextareaRows(textarea.rows);
+    };
+
     return (
         <div className="relative">
             <textarea
-                rows={3}
+                rows={textareaRows}
                 required
                 autoComplete="off"
                 id="newPost"
                 name="newPost"
-                className="peer placeholder-transparent w-full bg-blue-100 border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 p-4"
+                className={
+                    'peer placeholder-transparent w-full border-b-2 border-gray-300 text-gray-900 leading-tight focus:outline-none bg-blue-100 focus:bg-blue-200 p-2 h-full overflow-hidden resize-none'
+                }
                 placeholder="message"
                 value={postText}
-                onChange={handleNewPostChange}
+                onChange={handleTextareaChange}
+                onBlur={() => setIsTextareaEmpty(postText === '')}
+                style={{
+                    transition: 'background-color 0.5s ease-in-out',
+                }}
             />
             <label
                 htmlFor="newPost"
-                className="absolute left-0 -top-5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-5 peer-focus:text-gray-600 peer-focus:text-sm px-2"
+                className={`absolute left-0 text-gray-600 text-sm peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-3 transition-all ${
+                    isTextareaEmpty ? 'peer-focus:-top-5' : 'top-[-1.5rem]'
+                } peer-focus:text-gray-600 peer-focus:text-xs px-2`}
             >
-                what's on your mind, {username}?
+                What's on your mind, {username}?
             </label>
         </div>
     );
