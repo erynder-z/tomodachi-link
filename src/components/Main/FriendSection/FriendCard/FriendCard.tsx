adbd41f2977ff665463f7ae0ce562a7fd5fcc795
erynder-z <stfn.bgr@gmail.com>
2023-06-15 20:35:06 +0200

    Unfriend user on friend card

--- src/components/Main/FriendSection/FriendCard/FriendCard.tsx
+++ src/components/Main/FriendSection/FriendCard/FriendCard.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MinimalUserTypes } from '../../../../types/minimalUserTypes';
 import { getCorrectUserpicFormat } from '../../../../utilities/getCorrectUserpicFormat';
-import { TbLink, TbMessages, TbUserMinus } from 'react-icons/tb';
+import {
+    TbLink,
+    TbMessages,
+    TbUserMinus,
+    TbQuestionCircle,
+} from 'react-icons/tb';
 import { Link } from 'react-router-dom';
+import useAuth from '../../../../hooks/useAuth';
+import useCurrentUserData from '../../../../hooks/useCurrentUserData';
+import useInfoCard from '../../../../hooks/useInfoCard';
+import useDelayUnmount from '../../../../hooks/useDelayUnmount';
+import ConfirmationOverlay from '../../../ConfirmationOverlay/ConfirmationOverlay';
+import { unfriendUser } from '../../../../utilities/unfriendUser';
+import useFriendData from '../../../../hooks/useFriendData';
 
 type FriendCardProps = {
     friendData: MinimalUserTypes;
@@ -11,29 +23,70 @@ type FriendCardProps = {
 export default function FriendCard({ friendData }: FriendCardProps) {
     const { userpic, firstName, lastName, _id } = friendData;
 
+    const { token } = useAuth();
+    const { currentUserData, handleFetchUserData } = useCurrentUserData();
+    const { handleFetchFriendData } = useFriendData();
+    const { setInfo } = useInfoCard();
+    const [shouldConfirmDialogShow, setShouldConfirmDialogShow] =
+        useState(false);
+    const isConfirmDialogMounted = shouldConfirmDialogShow;
+    const showConfirmDialog = useDelayUnmount(isConfirmDialogMounted, 150);
+
+    const handleUnfriendButtonClick = () => {
+        setShouldConfirmDialogShow(true);
+    };
+
     return (
-        <div className="w-full lg:w-44 h-60">
-            <div className="w-full text-center p-4 bg-card shadow-lg">
-                <img
-                    className="w-20 h-20 object-cover rounded-full mx-auto shadow-lg"
-                    src={`data:image/png;base64,${getCorrectUserpicFormat(
-                        userpic
-                    )}`}
-                    alt="User avatar"
+        <>
+            {showConfirmDialog && (
+                <ConfirmationOverlay
+                    shouldConfirmDialogShow={shouldConfirmDialogShow}
+                    setShouldConfirmDialogShow={setShouldConfirmDialogShow}
+                    onConfirm={() => {
+                        if (token && currentUserData) {
+                            unfriendUser(
+                                token,
+                                currentUserData?._id,
+                                _id,
+                                handleFetchUserData,
+                                handleFetchFriendData,
+                                setInfo
+                            );
+                        }
+                    }}
+                    dialogInfo={{
+                        message: 'Do you really want to stop being friends?',
+                        icon: <TbQuestionCircle size="2em" />,
+                    }}
                 />
-                <p className="font-semibold text-md my-5 break-all">
-                    {firstName} {lastName}
-                </p>
-                <div className="flex justify-around items-center">
-                    <Link
-                        to={`/users/${_id}`}
-                        className="flex items-center w-max gap-4 py-2"
-                    >
-                        <TbLink size="1.5em" />
-                    </Link>
-                    <TbMessages size="1.5em" /> <TbUserMinus size="1.5em" />
+            )}
+            <div className="w-full lg:w-44 h-60">
+                <div className="w-full text-center p-4 bg-card shadow-lg">
+                    <img
+                        className="w-20 h-20 object-cover rounded-full mx-auto shadow-lg"
+                        src={`data:image/png;base64,${getCorrectUserpicFormat(
+                            userpic
+                        )}`}
+                        alt="User avatar"
+                    />
+                    <p className="font-semibold text-md my-5 break-all">
+                        {firstName} {lastName}
+                    </p>
+                    <div className="flex justify-around items-center">
+                        <Link
+                            to={`/users/${_id}`}
+                            className="flex items-center w-max gap-4 py-2"
+                        >
+                            <TbLink className="text-xl" />
+                        </Link>
+                        <TbMessages className="text-xl" />
+                        <TbUserMinus
+                            onClick={handleUnfriendButtonClick}
+                            className="text-xl cursor-pointer"
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
