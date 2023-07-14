2e336defe400a54a1d7ab7beda2fb8e54a91e176
erynder-z <stfn.bgr@gmail.com>
2023-07-14 13:30:40 +0200

    Show online chat users in sidebar during chat

--- src/components/Main/Sidebar/Sidebar.tsx
+++ src/components/Main/Sidebar/Sidebar.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import UserListSome from '../UserList/UserListSome/UserListSome';
 import { CurrentViewType } from '../../../types/currentViewType';
 import UserListAll from '../UserList/UserListAll/UserListAll';
+import OnlineUsersList from '../UserList/ChatOnlineUsersList/ChatOnlineUsersList';
+import { Socket } from 'socket.io-client';
 
 type SidebarProps = {
     currentView: CurrentViewType;
+    socket: Socket | undefined;
 };
 
-export default function Sidebar({ currentView }: SidebarProps) {
-    return currentView === 'Friends' ? <UserListAll /> : <UserListSome />;
+export default function Sidebar({ currentView, socket }: SidebarProps) {
+    if (currentView === 'Friends') {
+        return <UserListAll />;
+    } else if (currentView === 'Chat') {
+        return <OnlineUsersList socket={socket} />;
+    } else {
+        return <UserListSome />;
+    }
 }
