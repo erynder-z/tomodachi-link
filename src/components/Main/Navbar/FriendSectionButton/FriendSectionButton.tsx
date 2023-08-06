f0304671c5679424805809264217b7a6dfdad65c
erynder-z <stfn.bgr@gmail.com>
2023-08-06 14:21:28 +0200

    Remove unused code

--- src/components/Main/Navbar/FriendSectionButton/FriendSectionButton.tsx
+++ src/components/Main/Navbar/FriendSectionButton/FriendSectionButton.tsx
@@ -3,15 +3,12 @@ import { MdEmojiPeople } from 'react-icons/md';
 import { NavLink } from 'react-router-dom';
 
 export default function FriendSectionButton() {
-    const handleFriendSectionButtonClick = () => {
-        console.log('test');
-    };
     return (
         <NavLink
             to="/friends"
             className="flex self-center cursor-pointer text-cBlack h-full w-full"
         >
-            <button type="button" onClick={handleFriendSectionButtonClick}>
+            <button type="button">
                 <MdEmojiPeople size="1.5em" />
             </button>
         </NavLink>
--- src/components/Main/Navbar/HomeSectionButton/HomeSectionButton.tsx
+++ src/components/Main/Navbar/HomeSectionButton/HomeSectionButton.tsx
@@ -3,15 +3,12 @@ import { MdHomeFilled } from 'react-icons/md';
 import { NavLink } from 'react-router-dom';
 
 export default function HomeSectionButton() {
-    const handleHomeSectionButtonClick = () => {
-        console.log('test');
-    };
     return (
         <NavLink
             to="/home"
             className="flex self-center cursor-pointer text-cBlack h-full w-full"
         >
-            <button type="button" onClick={handleHomeSectionButtonClick}>
+            <button type="button">
                 <MdHomeFilled size="1.5em" />
             </button>
         </NavLink>
