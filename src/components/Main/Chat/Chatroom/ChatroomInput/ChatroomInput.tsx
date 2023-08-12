009105537ca7ad7fb3785962c1fdb0810cb7d99b
erynder-z <stfn.bgr@gmail.com>
2023-08-12 15:09:47 +0200

    Disable autocomplete

--- src/components/Main/Chat/Chatroom/ChatroomInput/ChatroomInput.tsx
+++ src/components/Main/Chat/Chatroom/ChatroomInput/ChatroomInput.tsx
@@ -30,6 +30,7 @@ export default function ChatroomInput({
                 type="text"
                 name="chatInput"
                 value={inputMessage}
+                autoComplete="off"
                 onChange={handleInputChange}
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 appearance-none focus:outline-none focus:ring-0 peer"
                 placeholder=" "
