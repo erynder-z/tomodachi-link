b0435950ed0814ecc42e7e49f7943f48627efaa0
erynder-z <stfn.bgr@gmail.com>
2023-08-15 20:31:01 +0200

    Update styling

--- src/components/Main/Post/NewPostInput/PostInputTextarea/PostInputTextarea.tsx
+++ src/components/Main/Post/NewPostInput/PostInputTextarea/PostInputTextarea.tsx
@@ -37,7 +37,7 @@ export default function PostInputTextarea({
                 autoComplete="off"
                 id="newPost"
                 name="newPost"
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0  peer overflow-hidden resize-none"
+                className="block py-2.5 px-0 w-full text-sm text-regularText dark:text-regularTextDark bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0  peer overflow-hidden resize-none"
                 placeholder=" "
                 value={postText}
                 onChange={handleTextareaChange}
