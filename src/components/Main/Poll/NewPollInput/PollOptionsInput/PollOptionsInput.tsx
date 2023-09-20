cd02fbe87f6d4eb23eec923a62572d9d94a880cc
erynder-z <stfn.bgr@gmail.com>
2023-09-20 21:15:19 +0200

    Refactor poll options input

--- src/components/Main/Poll/NewPollInput/PollOptionsInput/PollOptionsInput.tsx
+++ src/components/Main/Poll/NewPollInput/PollOptionsInput/PollOptionsInput.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import { PollDataType } from '../../../../../types/pollDataType';
 
 type PollOptionsInputProps = {
-    numberOfPollOptions: number;
-    pollOptions: string[];
-    onPollOptionChange: (index: number, value: string) => void;
+    pollData: PollDataType;
+    handlePollOptionInputChange: (index: number, value: string) => void;
 };
 
 export default function PollOptionsInput({
-    numberOfPollOptions,
-    pollOptions,
-    onPollOptionChange,
+    pollData,
+    handlePollOptionInputChange,
 }: PollOptionsInputProps) {
-    const options = [];
+    const { numberOfOptions, options } = pollData;
 
-    for (let i = 0; i <= numberOfPollOptions - 1; i++) {
-        options.push(
+    const optionElements = [];
+
+    for (let i = 0; i <= numberOfOptions - 1; i++) {
+        optionElements.push(
             <div key={i} className="relative z-0">
                 <input
                     required
@@ -23,8 +24,10 @@ export default function PollOptionsInput({
                     name={`pollOption${i}`}
                     className="block py-2.5 px-0 w-full text-sm text-regularText dark:text-regularTextDark bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 peer overflow-hidden resize-none"
                     placeholder=" "
-                    value={pollOptions[i]}
-                    onChange={(e) => onPollOptionChange(i, e.target.value)}
+                    value={options[i]}
+                    onChange={(e) =>
+                        handlePollOptionInputChange(i, e.target.value)
+                    }
                 />
                 <label
                     htmlFor={`pollOption${i}`}
@@ -37,5 +40,5 @@ export default function PollOptionsInput({
         );
     }
 
-    return <div className="flex flex-col gap-4">{options}</div>;
+    return <div className="flex flex-col gap-4">{optionElements}</div>;
 }
