b5127b9523c2a811845db32bd6fbea12dc5d7a83
erynder-z <stfn.bgr@gmail.com>
2023-10-09 21:50:15 +0200

    Refactor getting search excerpt

--- src/components/Search/SearchResults/SearchResultsPollItem/SearchResultsPollItem.tsx
+++ src/components/Search/SearchResults/SearchResultsPollItem/SearchResultsPollItem.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { SearchResultPollType } from '../../../../types/searchTypes';
+import { getExcerptWithHighlightedQuery } from '../../../../utilities/getExcerptWithHighlightedQuery ';
 
 type SearchResultsPollItemType = {
     itemData: SearchResultPollType;
@@ -11,45 +12,7 @@ export default function SearchResultsPollItem({
     queryString,
 }: SearchResultsPollItemType) {
     const { question, description } = itemData;
-
-    const getTextExcerpt = () => {
-        // Function to extract and highlight the matching query string.
-        const text = `${question} ${description}`; // Combine question and description
-        const query = queryString.toLowerCase();
-        const textLower = text.toLowerCase();
-        const queryIndex = textLower.indexOf(query);
-
-        if (queryIndex === -1) {
-            // If the query is not found in the text, return the first 35 characters.
-            return text.substring(0, 35) + '...';
-        } else {
-            const start = Math.max(0, queryIndex - 17); // 17 characters before and after query
-            const end = Math.min(text.length, start + 35); // 35 characters in total
-
-            // Extract the substring centered around the query.
-            const excerpt = text.substring(start, end);
-
-            // Split the excerpt into parts: before, matched, and after the query.
-            const beforeQuery = excerpt.substring(0, queryIndex - start);
-            const matchedQuery = excerpt.substring(
-                queryIndex - start,
-                queryIndex - start + query.length
-            );
-            const afterQuery = excerpt.substring(
-                queryIndex - start + query.length
-            );
-
-            return (
-                <>
-                    {start > 0 && '...'}
-                    {beforeQuery}
-                    <mark>{matchedQuery}</mark>
-                    {afterQuery}
-                    ...
-                </>
-            );
-        }
-    };
+    const pollText = `${question} ${description}`;
 
     return (
         <Link
@@ -58,7 +21,7 @@ export default function SearchResultsPollItem({
         >
             <div className="flex justify-center gap-4">
                 <div className="overflow-hidden whitespace-nowrap text-ellipsis">
-                    {getTextExcerpt()}
+                    {getExcerptWithHighlightedQuery(pollText, queryString)}
                 </div>
             </div>
             <div>Poll</div>
--- src/components/Search/SearchResults/SearchResultsPostItem/SearchResultsPostItem.tsx
+++ src/components/Search/SearchResults/SearchResultsPostItem/SearchResultsPostItem.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { SearchResultPostType } from '../../../../types/searchTypes';
+import { getExcerptWithHighlightedQuery } from '../../../../utilities/getExcerptWithHighlightedQuery ';
 
 type SearchResultsPostItemType = {
     itemData: SearchResultPostType;
@@ -12,42 +13,6 @@ export default function SearchResultsPostItem({
 }: SearchResultsPostItemType) {
     const { text } = itemData;
 
-    const getTextExcerpt = (text: string, queryString: string) => {
-        const queryIndex = text
-            .toLowerCase()
-            .indexOf(queryString?.toLowerCase());
-        if (queryIndex === -1) {
-            // If the queryString is not found in the text, return the first 35 characters.
-            return text.substring(0, 35) + '...';
-        } else {
-            const start = Math.max(0, queryIndex - 17); // 17 characters before and after queryString
-            const end = Math.min(text.length, start + 35); // 35 characters in total
-
-            // Extract the substring centered around the queryString.
-            const excerpt = text.substring(start, end);
-
-            // Split the excerpt into parts: before, matched, and after the query.
-            const beforeQuery = excerpt.substring(0, queryIndex - start);
-            const matchedQuery = excerpt.substring(
-                queryIndex - start,
-                queryIndex - start + queryString.length
-            );
-            const afterQuery = excerpt.substring(
-                queryIndex - start + queryString.length
-            );
-
-            return (
-                <>
-                    {start > 0 && '...'}
-                    {beforeQuery}
-                    <mark>{matchedQuery}</mark>
-                    {afterQuery}
-                    ...
-                </>
-            );
-        }
-    };
-
     return (
         <Link
             to="/"
@@ -55,7 +20,7 @@ export default function SearchResultsPostItem({
         >
             <div className="flex justify-center gap-4">
                 <div className="overflow-hidden whitespace-nowrap text-ellipsis">
-                    {getTextExcerpt(text, queryString)}
+                    {getExcerptWithHighlightedQuery(text, queryString)}
                 </div>
             </div>
             <div>Post</div>
