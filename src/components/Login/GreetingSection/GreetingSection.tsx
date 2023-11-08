00a6463fcdd674619fe7d2d9c079de27dc581f3f
erynder-z <stfn.bgr@gmail.com>
2023-11-08 20:20:42 +0100

    Simplify number animation count

--- src/components/Login/GreetingSection/GreetingSection.tsx
+++ src/components/Login/GreetingSection/GreetingSection.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import useInfoCard from '../../../hooks/useInfoCard';
 import { backendFetch } from '../../../utilities/backendFetch';
+import { motion, useMotionValue, useTransform, animate } from 'framer-motion';
 
 export default function GreetingSection() {
     const { setInfo } = useInfoCard();
@@ -8,32 +9,9 @@ export default function GreetingSection() {
 
     const shouldGetNumberOfUsers = useRef(true);
 
-    const formattedNumberOfUsers = numberOfUsers.toLocaleString(undefined, {
-        minimumIntegerDigits: 4,
-        useGrouping: false,
-    });
-
-    const animateCount = (from: number, to: number, duration: number) => {
-        const ANIMATION_INTERVAL = 10;
-        const steps = Math.ceil(duration / ANIMATION_INTERVAL);
-        const stepValue = (to - from) / steps;
-
-        let currentCount = from;
-        const animation = setInterval(() => {
-            currentCount += stepValue;
-            setNumberOfUsers(Math.round(currentCount));
-
-            if (Math.round(currentCount) >= to) {
-                clearInterval(animation);
-            }
-        }, ANIMATION_INTERVAL);
-    };
-
     const API_ENDPOINT_URL = '/api/v1/users/count';
     const METHOD = 'GET';
     const ERROR_MESSAGE = 'Unable to fetch number of users!';
-
-    const ANIMATION_DURATION = 500;
 
     const getNumberOfUsers = async () => {
         const token = undefined;
@@ -44,8 +22,19 @@ export default function GreetingSection() {
             METHOD,
             ERROR_MESSAGE
         );
-        animateCount(0, response?.numberOfUsers, ANIMATION_DURATION);
+        setNumberOfUsers(response?.numberOfUsers);
     };
+
+    const count = useMotionValue(0);
+    const rounded = useTransform(count, Math.round);
+
+    useEffect(() => {
+        if (numberOfUsers !== 0) {
+            const animation = animate(count, numberOfUsers, { duration: 1 });
+
+            return animation.stop;
+        }
+    }, [numberOfUsers]);
 
     useEffect(() => {
         if (shouldGetNumberOfUsers.current) getNumberOfUsers();
@@ -59,7 +48,7 @@ export default function GreetingSection() {
         <section className="text-white font-bold font-mono md:text-3xl text-center bg-cBlack/75 p-4 lg:rounded-full w-5/6 mx-auto">
             <h1>Welcome to Odin-Book!</h1>
             Join our community of{' '}
-            <span className="text-cCyan">{formattedNumberOfUsers}</span> users!
+            <motion.span className="text-cCyan">{rounded}</motion.span> users!
         </section>
     );
 }
