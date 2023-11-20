2b882043bbce72a6113dfd5782abacc700fd6995
erynder-z <stfn.bgr@gmail.com>
2023-11-20 20:52:54 +0100

    Adjust UI

--- src/components/Main/PollSection/NewPollSection/NewPollSection.tsx
+++ src/components/Main/PollSection/NewPollSection/NewPollSection.tsx
@@ -8,9 +8,9 @@ export default function NewPollSection() {
             animate={{ y: 0, opacity: 1 }}
             exit={{ y: -10, opacity: 0 }}
             transition={{ duration: 0.2 }}
-            className="flex flex-col min-h-[calc(100vh_-_3rem)] lg:min-h-full lg:p-4 md:p-0 pb-4 bg-background2 dark:bg-background2Dark text-regularText dark:text-regularTextDark shadow-lg rounded lg:rounded-lg"
+            className="flex flex-col items-center min-h-[calc(100vh_-_3rem)] lg:min-h-full lg:p-4 md:p-0 pb-4 bg-background2 dark:bg-background2Dark text-regularText dark:text-regularTextDark shadow-lg rounded lg:rounded-lg"
         >
-            <h1 className="text-center text-xl font-bold mb-4">
+            <h1 className="flex justify-center gap-2 text-xl font-bold sticky top-0 mb-4  px-4 w-fit rounded-full bg-gray-300/80 dark:bg-gray-500/80">
                 Create new poll
             </h1>
             <NewPollInput />
