04d0a9ebd4f33ae1051c0161e40844462182a3e3
erynder-z <stfn.bgr@gmail.com>
2023-12-09 16:17:19 +0100

    Fix some possible null pointers

--- src/utilities/getExcerptWithHighlightedQuery.tsx
+++ src/utilities/getExcerptWithHighlightedQuery.tsx
@@ -3,24 +3,26 @@ export const getExcerptWithHighlightedQuery = (
     query: string,
     maxLength = 35
 ) => {
-    const queryIndex = text.toLowerCase().indexOf(query?.toLowerCase());
+    const queryIndex = text?.toLowerCase().indexOf(query?.toLowerCase());
     if (queryIndex === -1) {
         // If the query is not found in the text, return the first maxLength characters.
-        return text.substring(0, maxLength) + '...';
+        return text?.substring(0, maxLength) + '...';
     } else {
         const start = Math.max(0, queryIndex - 17); // 17 characters before and after query
-        const end = Math.min(text.length, start + maxLength); // maxLength characters in total
+        const end = Math.min(text?.length, start + maxLength); // maxLength characters in total
 
         // Extract the substring centered around the query.
-        const excerpt = text.substring(start, end);
+        const excerpt = text?.substring(start, end);
 
         // Split the excerpt into parts: before, matched, and after the query.
-        const beforeQuery = excerpt.substring(0, queryIndex - start);
-        const matchedQuery = excerpt.substring(
+        const beforeQuery = excerpt?.substring(0, queryIndex - start);
+        const matchedQuery = excerpt?.substring(
             queryIndex - start,
-            queryIndex - start + query.length
+            queryIndex - start + query?.length
         );
-        const afterQuery = excerpt.substring(queryIndex - start + query.length);
+        const afterQuery = excerpt?.substring(
+            queryIndex - start + query?.length
+        );
 
         return (
             <>
