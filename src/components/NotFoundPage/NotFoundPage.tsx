bd2fd973385fef197c3e985fc66128f5f918de2c
erynder-z <stfn.bgr@gmail.com>
2024-01-29 20:23:53 +0100

    Add JSDoc for 404 page component

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -1,3 +1,10 @@
+/**
+ * React component representing the Not Found page.
+ *
+ * @component
+ * @returns {JSX.Element} The rendered NotFoundPage component.
+ */
+
 export default function NotFoundPage() {
     return <div className="w-full bg-black text-white">Not Found</div>;
 }
