a8077e0d0c325fd2fe35278b9271f830f323a30f
erynder-z <stfn.bgr@gmail.com>
2024-02-29 21:16:58 +0100

    Add feed help content

--- src/components/Main/Help/HelpContent/FeedHelp.tsx
+++ src/components/Main/Help/HelpContent/FeedHelp.tsx
@@ -3,19 +3,20 @@ export default function FeedHelp() {
         <article className="block">
             <h2 className="text-xl">The feed</h2>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-                facilis odio iusto quas blanditiis vero quo cupiditate
-                voluptatibus dolor, nam incidunt, nesciunt nemo alias laboriosam
-                consectetur repudiandae est doloribus assumenda? Dolorum ratione
-                magni nostrum aspernatur sunt minima, magnam culpa! Consectetur
-                fuga, quo minima accusamus, omnis temporibus laudantium
-                molestias aut sunt doloribus fugiat esse, ipsa odio. Dolorem,
-                corporis! Sed vero ducimus quo porro qui? Eaque beatae totam,
-                eum a rem, iste unde eveniet quae ut molestias quidem accusamus
-                neque dolorem in quasi tenetur reiciendis sapiente? Nemo fugit
-                omnis reiciendis a? Adipisci culpa, voluptate fugiat obcaecati
-                nisi quibusdam! Corrupti, aperiam? Magni, amet?
+                The Feed aggregates the most recent posts from your friends,
+                ensuring you never miss out on their latest activities. Scroll
+                through to see what your connections have been up to.
             </p>
+            <p>
+                As you navigate through the Feed, older posts will automatically
+                load, allowing you to scroll endlessly and catch up on any
+                updates you might have missed.
+            </p>
+            <p>
+                All users, currently featured in the feed will be listed, so you
+                always know who has been active lately.
+            </p>
+            <p>You can also add new posts directly from the feed.</p>
         </article>
     );
 }
