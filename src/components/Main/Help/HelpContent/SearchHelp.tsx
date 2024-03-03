b05f01655e2ca0704a1d3e9e100b4d042b906a81
erynder-z <stfn.bgr@gmail.com>
2024-03-03 17:41:17 +0100

    Add search help content

--- src/components/Main/Help/HelpContent/SearchHelp.tsx
+++ src/components/Main/Help/HelpContent/SearchHelp.tsx
@@ -3,18 +3,23 @@ export default function SearchHelp() {
         <article className="block">
             <h2 className="text-xl">Searching</h2>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-                facilis odio iusto quas blanditiis vero quo cupiditate
-                voluptatibus dolor, nam incidunt, nesciunt nemo alias laboriosam
-                consectetur repudiandae est doloribus assumenda? Dolorum ratione
-                magni nostrum aspernatur sunt minima, magnam culpa! Consectetur
-                fuga, quo minima accusamus, omnis temporibus laudantium
-                molestias aut sunt doloribus fugiat esse, ipsa odio. Dolorem,
-                corporis! Sed vero ducimus quo porro qui? Eaque beatae totam,
-                eum a rem, iste unde eveniet quae ut molestias quidem accusamus
-                neque dolorem in quasi tenetur reiciendis sapiente? Nemo fugit
-                omnis reiciendis a? Adipisci culpa, voluptate fugiat obcaecati
-                nisi quibusdam! Corrupti, aperiam? Magni, amet?
+                The typeahead search feature provides real-time suggestions as
+                you type, helping you find what you're looking for quickly and
+                efficiently. Simply start typing a keyword, and relevant results
+                will appear instantly, making it effortless to discover users,
+                posts, and polls.
+            </p>
+            <p>
+                By default, the search function scours through users, posts, and
+                polls, ensuring that you have access to a wide range of content
+                and connections.
+            </p>
+            <p>
+                You can also narrow down your search results by clicking on the
+                corresponding tab in the search overlay to limit your search to
+                include only users, posts, or polls. This allows you to focus
+                your search on the type of content or connections that matter
+                most to you.
             </p>
         </article>
     );
