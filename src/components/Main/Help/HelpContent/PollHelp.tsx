c6c71bc44a0190e2551e9822c4d90865e2ecaebe
erynder-z <stfn.bgr@gmail.com>
2024-03-03 17:41:11 +0100

    Add poll help content

--- src/components/Main/Help/HelpContent/PollHelp.tsx
+++ src/components/Main/Help/HelpContent/PollHelp.tsx
@@ -3,18 +3,28 @@ export default function PollHelp() {
         <article className="block">
             <h2 className="text-xl">Polls</h2>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-                facilis odio iusto quas blanditiis vero quo cupiditate
-                voluptatibus dolor, nam incidunt, nesciunt nemo alias laboriosam
-                consectetur repudiandae est doloribus assumenda? Dolorum ratione
-                magni nostrum aspernatur sunt minima, magnam culpa! Consectetur
-                fuga, quo minima accusamus, omnis temporibus laudantium
-                molestias aut sunt doloribus fugiat esse, ipsa odio. Dolorem,
-                corporis! Sed vero ducimus quo porro qui? Eaque beatae totam,
-                eum a rem, iste unde eveniet quae ut molestias quidem accusamus
-                neque dolorem in quasi tenetur reiciendis sapiente? Nemo fugit
-                omnis reiciendis a? Adipisci culpa, voluptate fugiat obcaecati
-                nisi quibusdam! Corrupti, aperiam? Magni, amet?
+                In the polls section of Tomodachi-Link, you can engage with your
+                friends and community by creating and participating in polls.
+            </p>
+            <p>
+                Easily create polls by entering a question and providing up to 6
+                possible answer options. Whether you're seeking opinions,
+                gathering feedback, or sparking discussions, polls are a fun and
+                interactive way to engage with other users.
+            </p>
+            <p>
+                You can tailor polls to suit your preferences and privacy needs.
+                You have the option to make your polls friends-only, ensuring
+                that only your trusted connections can participate.
+                Additionally, you can enable a comment section for each poll,
+                allowing users to share their thoughts and engage in discussions
+                related to the poll topic.
+            </p>
+            <p>
+                Poll results are visually displayed as either a pie chart or a
+                bar chart, making it easy to interpret and analyze the data.
+                Gain insights into the collective opinions and preferences of
+                the community.
             </p>
         </article>
     );
