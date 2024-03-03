6351aec25e8f93a0f7434548ee993532d1a8a788
erynder-z <stfn.bgr@gmail.com>
2024-03-03 17:41:31 +0100

    Add user page help content

--- src/components/Main/Help/HelpContent/UserPageHelp.tsx
+++ src/components/Main/Help/HelpContent/UserPageHelp.tsx
@@ -3,18 +3,13 @@ export default function UserPageHelp() {
         <article className="block">
             <h2 className="text-xl">The user page</h2>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-                facilis odio iusto quas blanditiis vero quo cupiditate
-                voluptatibus dolor, nam incidunt, nesciunt nemo alias laboriosam
-                consectetur repudiandae est doloribus assumenda? Dolorum ratione
-                magni nostrum aspernatur sunt minima, magnam culpa! Consectetur
-                fuga, quo minima accusamus, omnis temporibus laudantium
-                molestias aut sunt doloribus fugiat esse, ipsa odio. Dolorem,
-                corporis! Sed vero ducimus quo porro qui? Eaque beatae totam,
-                eum a rem, iste unde eveniet quae ut molestias quidem accusamus
-                neque dolorem in quasi tenetur reiciendis sapiente? Nemo fugit
-                omnis reiciendis a? Adipisci culpa, voluptate fugiat obcaecati
-                nisi quibusdam! Corrupti, aperiam? Magni, amet?
+                In addition to a short description, the user sage shows displays
+                all the posts from the selected user, as their friends and
+                photos.
+            </p>
+            <p>
+                On your own user page, you can also change your cover image, add
+                new posts and accept/decline incoming fiend requests.
             </p>
         </article>
     );
