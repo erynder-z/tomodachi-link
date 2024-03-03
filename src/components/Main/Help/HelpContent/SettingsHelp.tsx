ede592043b0d63b4fe08163450d3fb345de87b69
erynder-z <stfn.bgr@gmail.com>
2024-03-03 17:41:23 +0100

    Add settings help content

--- src/components/Main/Help/HelpContent/SettingsHelp.tsx
+++ src/components/Main/Help/HelpContent/SettingsHelp.tsx
@@ -5,18 +5,12 @@ export default function SettingsHelp() {
         <article className="block">
             <h2 className="text-xl">Settings</h2>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-                facilis odio iusto quas blanditiis vero quo cupiditate
-                voluptatibus dolor, nam incidunt, nesciunt nemo alias laboriosam
-                consectetur repudiandae est doloribus assumenda? Dolorum ratione
-                magni nostrum aspernatur sunt minima, magnam culpa! Consectetur
-                fuga, quo minima accusamus, omnis temporibus laudantium
-                molestias aut sunt doloribus fugiat esse, ipsa odio. Dolorem,
-                corporis! Sed vero ducimus quo porro qui? Eaque beatae totam,
-                eum a rem, iste unde eveniet quae ut molestias quidem accusamus
-                neque dolorem in quasi tenetur reiciendis sapiente? Nemo fugit
-                omnis reiciendis a? Adipisci culpa, voluptate fugiat obcaecati
-                nisi quibusdam! Corrupti, aperiam? Magni, amet?
+                The settings menu allows you to update your user information,
+                such as your name, the about-text or email-address.
+            </p>
+            <p>
+                You can also change your user picture as well as change your
+                current password from the settings menu.
             </p>
         </article>
     );
