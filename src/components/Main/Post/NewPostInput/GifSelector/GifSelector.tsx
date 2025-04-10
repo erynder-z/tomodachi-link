1631e931cf01d9dce18bce60719a90417fce2224
erynder-z <stfn.bgr@gmail.com>
2025-04-10 08:34:31 +0200

    Fix choppy animation on firefox

--- src/components/Main/Post/NewPostInput/GifSelector/GifSelector.tsx
+++ src/components/Main/Post/NewPostInput/GifSelector/GifSelector.tsx
@@ -58,13 +58,7 @@ export default function GifSelector({
      * @type {JSX.Element}
      */
     return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            viewport={{ once: true }}
-            className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden  flex flex-col items-center justify-center gap-4 transition-opacity bg-gray-800/80"
-        >
+        <motion.div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden  flex flex-col items-center justify-center gap-4 transition-opacity bg-gray-800/80">
             <div className="relative">
                 {CloseButton}
 
